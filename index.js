b07df36456a13a12f27fced9bfc1a5a2c2a1f440
JeeWayne <jeewayne11@gmail.com>
2022-07-08 23:37:01 +0800

    Added JSDoc support.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const VendrAPI = require('./lib/Vendr/VendrAPI');
+//const VendrAPI = require('./lib/Vendr/VendrAPI');
+const VendrAPIV1 = require('./lib/Vendr/VendrV1/VendrAPIV1');
+const VendrAPIV2 = require('./lib/Vendr/VendrV2/VendrAPIV2');
+const VendrAPI = require('./lib/Vendr/VendrV2/VendrAPIV2');
 
-
-module.exports = {VendrAPI}
+module.exports = {VendrAPI, VendrAPIV1, VendrAPIV2}
